0d05c0aa5f1501fb580e2f1c09c2ae1ded0e89b2
Jerome <1030155707@qq.com>
2020-02-25 12:30:13 +0800

    fix novel

--- novel/urls.py
+++ novel/urls.py
@@ -6,6 +6,7 @@ from novel import views
 urlpatterns = [
     path('home/', views.home),
     path('chapter/<int:a_id>.html', views.chapter),
+    path('chapter/<int:a_id>/<int:c_id>.html', views.chapter),
     path('category/<int:a_id>.html', views.category),
     path('search/', views.search),
     path('art/getBookText/', views.get_book_text),
--- novel/views.py
+++ novel/views.py
@@ -19,8 +19,8 @@ def get_json_data(url):
 
 def search(request):
     wd = request.GET.get('wd')
-    if wd=='':
-        wd=' '
+    if wd == '':
+        wd = ' '
     url = "https://sou.jiaston.com/search.aspx?key=%s&siteid=app2" % wd
     book_list = get_json_data(url)
     return render(request, 'novel_search.html', {'book_list': book_list, 'wd': wd})
@@ -34,11 +34,12 @@ def category(request, a_id):
     return render(request, 'novel_category.html', {'cate_list': cate_list, 'book_name': book_name, 'a_id': a_id})
 
 
-def chapter(request, a_id):
-    cate_url = 'https://iosapp.canrike.com/book/%s/' % a_id
-    cate_data = get_json_data(cate_url)
-    first_c_id = cate_data['list'][0]['list'][0]['id']
-    url = 'https://iosapp.canrike.com/book/%s/%s.html' % (a_id, first_c_id)
+def chapter(request, a_id, c_id=-1):
+    if c_id == -1:
+        cate_url = 'https://iosapp.canrike.com/book/%s/' % a_id
+        cate_data = get_json_data(cate_url)
+        c_id = cate_data['list'][0]['list'][0]['id']
+    url = 'https://iosapp.canrike.com/book/%s/%s.html' % (a_id, c_id)
     data = get_json_data(url)
     return render(request, 'novel_chapter.html', {'art_data': data})
 
