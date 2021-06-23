d05f10ec765fc2a00781c921c8554dc004e64c9d
张俊龙 <zhangjl38@midea.com>
2021-06-23 18:00:03 +0800

    添加埋点

--- blog/templates/work_test/js/report.js
+++ blog/templates/work_test/js/report.js
@@ -83,6 +83,26 @@ function getsecondTab() {
     showDetail(HeiTou);
 
 }
+/**
+ * 事件埋点
+ * @param param
+ */
+function eventTracking(param=""){
+  let self = this
+  if (this.isMeiju) {
+    let eventTrackingParams = {
+      event: 'plugin_button_click',
+      eventParams: {
+        module: '插件',
+        bd_name: '厨热',
+        page_name:  `肌肤管家`,
+        element_content: param}
+    }
+
+    window.mdSmartios.bridge.trackEvent(eventTrackingParams)
+  } 
+}
+
 function showDetail(data){
     $(".now").css("left",(100-data.score)+"%");
     $(".pro_desc").html(data.description);
@@ -97,12 +117,14 @@ function showDetail(data){
     var prodata= data.product;
     for(var i=0;i<prodata.length;i++){
         pro_html+='<div class="recm_list flexCon"><div class="recm_pic"> <img src="'+prodata[i].image+'"/> </div><div class="recm_det flexCon"><div class="recm_det_name">'+prodata[i].name
-        +'</div><div class="recm_det_tag"><span>过滤余氟</span><span>无味</span></div><a class="recm_det_buy flexCon" onclick="tobuy('+prodata[i].url+')"> 去抢购</a></div></div>';
+        +'</div><div class="recm_det_tag"><span>过滤余氟</span><span>无味</span></div><a class="recm_det_buy flexCon" onclick="tobuy('+"'"+prodata[i].url+"'"+')"> 去抢购</a></div></div>';
     }
     $(".recm_title").after(pro_html);
     
 }
 
 function tobuy(url){
-    window.mdSmartios.bridge.goToMeijuPage("jumpWebview",{url:url})
+    url = "https://www.midea.cn/10162/1000000000100511173930.html"
+    window.mdSmartios.bridge.goToMeijuPage("jumpElecBusiness",{"url":url})
+    eventTracking("去购买")
 }
